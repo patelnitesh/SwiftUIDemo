046fd1a27a1b2a1aabcb75b458e25b3359359463
Nitesh Patel <no@email.com>
2022-04-28 11:55:34 +0100

    Start of Project

---
+++ SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by Nitesh Patel on 28/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
